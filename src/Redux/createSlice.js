704aaea44a7ca447ac2157c7eba7ff4af305c534
IsaienkoViktor <isaenkov1986@gmail.com>
2023-07-12 09:41:30 +0300

    add

--- src/Redux/createSlice.js
+++ src/Redux/createSlice.js
@@ -63,5 +63,3 @@ const filteredSlice = createSlice({
 export const contactSlice = contactsSlice.reducer;
 export const { setFilter } = filteredSlice.actions;
 export const filterSlice = filteredSlice.reducer;
-
-// tect
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,17 +1,9 @@
 import { ContactItem } from 'components/ContactItem/ContactItem';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'Redux/selectors';
+import { selectFilteredContact } from 'Redux/selectors';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-
-  const getFilterContact = () => {
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(filter.toLocaleLowerCase())
-    );
-  };
-  const filteredContacts = getFilterContact();
+  const filteredContacts = useSelector(selectFilteredContact);
 
   return (
     <ul>
